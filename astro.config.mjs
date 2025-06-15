e448eb9aed19910efb2f816dbbf12ed2fcd2559d
aytony <anyang@buaa.edu.cn>
2025-06-16 03:42:23 +0800

    feat: 添加 md 文章对应的 layout 并应用，添加 typ 对应的组件和测试页面

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { typst } from 'astro-typst';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    typst({
+      options: {
+        remPx: 14,
+      },
+      target: (id) => {
+        console.debug(`Detecting ${id}`);
+        if (id.endsWith('.html.typ') || id.includes('/html/'))
+          return "html";
+        return "svg";
+      },
+    }),
+  ],
+});
---
+++ src/pages/posts/test2.typ
@@ -0,0 +1,17 @@
+#metadata((
+  title: "Test page",
+  author: "Neko",
+  desc: [$oo$ fun with `math`],
+  date: datetime(
+    year: 2024,
+    month: 8,
+    day: 7,
+  ),
+  layout: "/src/layouts/post-layout.astro",
+))<frontmatter>
+
+test typst
+
+$integral.dash_omega f$
+
+$ integral.dash_omega f $

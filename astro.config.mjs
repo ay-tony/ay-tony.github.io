0ef78384dc4bdb71b1ee32072136402428ff9908
aytony <shuijingty@outlook.com>
2025-06-15 21:57:58 +0000

    ci: 合并 dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { typst } from 'astro-typst';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    typst({
+      options: {
+        remPx: 14,
+      },
+      target: (id) => {
+        console.debug(`Detecting ${id}`);
+        if (id.endsWith('.html.typ') || id.includes('/html/'))
+          return "html";
+        else if (id.endsWith(".svg.typ") || id.includes("/svg/"))
+          return "svg";
+        else
+          return "html";
+      },
+    }),
+  ],
+});

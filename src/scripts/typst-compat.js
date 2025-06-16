836f80de884170393da2cbf49e706951afa1d4d6
aytony <anyang@buaa.edu.cn>
2025-06-16 21:38:13 +0800

    refactor: 把嵌入 typst 修改的 js 代码集合在一起

--- src/scripts/typst-compat.js
+++ src/scripts/typst-compat.js
@@ -1,10 +1,26 @@
+// typst.math 适配
 document.addEventListener("DOMContentLoaded", function () {
     const groups = document.querySelectorAll("svg g.typst-text");
-
     groups.forEach((group) => {
         const useElements = group.querySelectorAll("use");
         useElements.forEach((useEl) => {
             useEl.setAttribute("fill", "currentColor");
+        });
+    });
+});
+
+// typst.html.frame 适配
+document.addEventListener("DOMContentLoaded", function() {
+    const svgElements = document.querySelectorAll("svg.typst-doc");
+    const styles = {
+        width: "auto",
+        height: "auto",
+        maxWidth: "100%",
+        maxHeight: "100%"
+    };
+    svgElements.forEach(svg => {
+        Object.entries(styles).forEach(([property, value]) => {
+            svg.style[property] = value;
         });
     });
 });

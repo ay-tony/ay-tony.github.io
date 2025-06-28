cf99d7f32c5d3c9d73bad0ec7dc61275b4450897
aytony <anyang@buaa.edu.cn>
2025-06-28 10:21:20 +0800

    feat: 添加 sitemap 集成，准备提交网站到 Google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { typst } from 'astro-typst';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://solitairemiya.cn",
   vite: {
     ssr:{
       external: ["@myriaddreamin/typst-ts-node-compiler"],
     },
   },
-  integrations: [
-    typst({
-      options: {
-        remPx: 14,
-      },
-      target: (id) => {
-        console.debug(`Detecting ${id}`);
-        if (id.endsWith('.html.typ') || id.includes('/html/'))
-          return "html";
-        else if (id.endsWith(".svg.typ") || id.includes("/svg/"))
-          return "svg";
-        else
-          return "html";
-      },
-    }),
-  ],
+  integrations: [typst({
+    options: {
+      remPx: 14,
+    },
+    target: (id) => {
+      console.debug(`Detecting ${id}`);
+      if (id.endsWith('.html.typ') || id.includes('/html/'))
+        return "html";
+      else if (id.endsWith(".svg.typ") || id.includes("/svg/"))
+        return "svg";
+      else
+        return "html";
+    },
+  }), sitemap()],
 });
